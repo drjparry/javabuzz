583a172e76a8a1b8ea684f0a58e6109ff058d4da
Iryna Howarth <irynahowarth@gmail.com>
2016-01-15 11:55:41 +0000

    passed 'buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it "should be 'fizz' when passed 3" do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it "should return 'buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
