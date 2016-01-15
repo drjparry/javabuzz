8b039d525248f8578eb5faf247eefa8419d858c1
Iryna Howarth <irynahowarth@gmail.com>
2016-01-15 12:09:48 +0000

    changed to class

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,15 +2,15 @@ require 'fizzbuzz'
 
 describe 'fizzbuzz' do
   it "should be 'fizz' when passed 3" do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
   it "should return 'buzz' when passed 5" do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(5.fizzbuzz).to eq 'buzz'
   end
   it "should return 'fizzbuzz' when passe 15" do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
   it "should return number when pass any other number" do
-    expect(fizzbuzz(4)).to eq 4
+    expect(4.fizzbuzz).to eq 4
   end
 end
