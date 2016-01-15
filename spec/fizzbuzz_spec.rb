4e44d9de2b180024d71a335bef89ca17f7589cb1
Iryna Howarth <irynahowarth@gmail.com>
2016-01-15 12:03:42 +0000

    added other numbers

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,7 @@ describe 'fizzbuzz' do
   it "should return 'fizzbuzz' when passe 15" do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  it "should return number when pass any other number" do
+    expect(fizzbuzz(4)).to eq 4
+  end
 end
